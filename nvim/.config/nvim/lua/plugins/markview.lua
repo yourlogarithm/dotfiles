d5de341bd6b4070b771686d360c4afef5a4e2466
Vlad Sebastian CRETU <vcretu@bitdefender.com>
2026-07-02 13:38:06 +0300

    markview

---
+++ nvim/.config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "OXY2DEV/markview.nvim",
+    -- Load after colorscheme so highlight groups resolve correctly (plugin docs).
+    lazy = false,
+    -- Needs the `markdown` + `markdown_inline` treesitter parsers (see treesitter.lua).
+  },
+}
